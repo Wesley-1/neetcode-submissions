4eaf314c9f68af6dc0a8bdce0931805c4420ebc0
Wesley <81816734+Wesley-1@users.noreply.github.com>
2026-08-05 17:23:30 -0400

    Add: custom-sort-string - submission-0

---
+++ Data Structures & Algorithms/custom-sort-string/submission-0.java
@@ -0,0 +1,42 @@
+class Solution {
+    public String customSortString(String order, String s) {
+        final Map<Character, Integer> indexes = new HashMap();
+
+        for (int i = 0; i < order.length(); i++) indexes.put(
+            order.charAt(i), 
+            Math.min(indexes.getOrDefault(order.charAt(i), i), i)
+        );
+
+        final Comparator<Character> comparator = (a, b) -> {
+
+            return Integer.compare(indexes.getOrDefault(a, Integer.valueOf(a)), indexes.getOrDefault(b, Integer.valueOf(b)));
+        };
+
+        final Map<Character, Integer> freq = new TreeMap(comparator);
+
+        for (char c : s.toCharArray()) { 
+            System.out.println(c);
+            freq.put(
+            c,
+            freq.getOrDefault(c, 0) + 1
+        );
+        }
+
+        System.out.println(freq);
+
+        final StringBuilder builder = new StringBuilder();
+
+        for (Map.Entry<Character, Integer> e : freq.entrySet()) {
+            final int f = e.getValue();
+            final char c = e.getKey();
+
+            System.out.println(f);
+
+            for (int i = 0; i < f; i++) {
+                builder.append(c);
+            }
+        }
+
+        return builder.toString();
+    }
+}

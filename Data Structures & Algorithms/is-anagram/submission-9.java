0d86baf79a34ec3bd5b4af0ff4cc610c19365c92
Wesley <81816734+Wesley-1@users.noreply.github.com>
2026-08-05 11:10:51 -0400

    Add: is-anagram - submission-9

---
+++ Data Structures & Algorithms/is-anagram/submission-9.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+       if (s.length() != t.length()) return false;
+
+       final Map<Character, Integer> sm = new HashMap();
+       final Map<Character, Integer> tm = new HashMap();
+
+       for (int i = 0; i < s.length(); i++) {
+        sm.put(s.charAt(i), sm.getOrDefault(s.charAt(i), 0) + 1);
+        tm.put(t.charAt(i), tm.getOrDefault(t.charAt(i), 0) + 1);
+       }
+
+       return sm.equals(tm);
+    }
+}

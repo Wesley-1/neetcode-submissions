eefc4da03d18b007950fff276df8e669b823fc56
Wesley <81816734+Wesley-1@users.noreply.github.com>
2026-08-17 20:56:30 -0400

    Add: meeting-schedule - submission-5

---
+++ Data Structures & Algorithms/meeting-schedule/submission-5.java
@@ -0,0 +1,26 @@
+/**
+ * Definition of Interval:
+ * public class Interval {
+ *     public int start, end;
+ *     public Interval(int start, int end) {
+ *         this.start = start;
+ *         this.end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+    public boolean canAttendMeetings(List<Interval> intervals) {
+        if (intervals.size() == 0) return true;   
+        intervals.sort((a, b) -> Integer.compare(a.start, b.start));
+
+        Interval prev = intervals.get(0);
+        for (int i = 1; i < intervals.size(); i++) {
+            if (prev.end > intervals.get(i).start)
+                return false;
+            prev = intervals.get(i);
+        }
+
+        return true;
+    }
+}

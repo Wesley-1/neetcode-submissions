16004a153e6f39731c5f6dc3391ee12c69781df1
Wesley <81816734+Wesley-1@users.noreply.github.com>
2026-08-19 17:34:49 -0400

    Add: dynamicArray - submission-5

---
+++ Data Structures & Algorithms/dynamicArray/submission-5.java
@@ -0,0 +1,54 @@
+class DynamicArray {
+    private int[] arr;
+    private int size;
+    
+    public DynamicArray(int capacity) {
+        arr = new int[capacity];
+        size = 0;
+    }
+
+    public int get(int i) {
+        if(i < size) {
+            return arr[i];
+        }
+        else {
+            return -1;
+        }
+    }
+
+    public void set(int i, int n) {
+        arr[i] = n;
+    }
+
+    public void pushback(int n) {
+        if(size == arr.length) {
+            resize();
+        }
+
+        arr[size] = n;
+        size++;
+    }
+
+    public int popback() {
+        size--;
+        return arr[size];
+    }
+
+    private void resize() {
+        int[] arr2 = new int[getCapacity() * 2];
+
+        for(int i = 0; i < size; i++) {
+            arr2[i] = arr[i];
+        }
+
+        arr = arr2;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public int getCapacity() {
+        return arr.length;
+    }
+}
